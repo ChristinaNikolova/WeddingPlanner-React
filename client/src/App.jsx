e6a7f370c85a356c4d6d2f23e536b4422fa87ab9
christinanikolova <christinanikolova@github.com>
2022-11-01 14:18:09 +0200

    reset css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,35 @@ import './App.css';
 function App() {
   return (
     <div>
-      <header>Test</header>
+      {/* <header className='header'>
+        <nav className="header-nav">
+          <ul className="header-nav-ul">
+            <a>Blog</a>
+            <a>Planner</a>
+          </ul>
+        </nav>
+        <nav className="header-nav">
+          <ul className="header-nav-ul">
+            <li className="header-nav-li logo">
+              <a>WeddingPlanner</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="header-nav">
+          <ul className="header-nav-ul">
+            <a>Login</a>
+            <a>Register</a>
+          </ul>
+        </nav>
+      </header> */}
+      <form action="">
+        <input type="text" name="firstName" />
+        <input type="text" name="lastName" />
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <input type="password" name="repass" />
+        <button>Register</button>
+      </form>
     </div>
   );
 }
