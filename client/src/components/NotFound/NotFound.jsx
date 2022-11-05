19407c4ea6ef4f6de61436484fd95762ca7c51fe
christinanikolova <christinanikolova@github.com>
2022-11-05 12:02:18 +0200

    add not found page

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+import styles from './NotFound.module.css';
+
+
+
+//className={styles["header"]}
+//{`${styles["header-nav-li"]} logo gold-underline`}
+
 function NotFound() {
-    return (<h2>Not Found</h2>);
+    return (
+        <section className={`${styles["not-found"]} section`}>
+            <h2 className={styles["not-found-main-title"]}>404</h2>
+            <h4 className={styles["not-found-sub-title"]}>Page not found</h4>
+            <p className={styles["not-found-content"]}>
+                Sorry, the page you are looking for doesn't exist
+            </p>
+            <Link to="/" className="btn">Go back to home</Link>
+        </section>
+    );
 }
 
 export default NotFound;
