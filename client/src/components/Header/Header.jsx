debffcf2275fbcd109d7045cd4c66bb0bf111edb
christinanikolova <christinanikolova@github.com>
2022-11-05 11:05:18 +0200

    add active nav links

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 function Header() {
+    const setNavStyle = ({ isActive }) => {
+        return isActive ? styles["header-active-li"] : undefined;
+    }
+
     return (
         <header className={styles.header}>
             <nav className={styles["header-nav"]}>
                 <ul className={styles["header-nav-ul"]}>
                     <li className={styles["header-nav-li"]}>
-                        <Link to="/">Plan your wedding</Link>
+                        <NavLink className={setNavStyle} to="/plan">Plan your wedding</NavLink>
                     </li>
                     <li className={styles["header-nav-li"]}>
-                        <Link to="/">Wedding's blog</Link>
+                        <NavLink className={setNavStyle} to="/blog">Wedding's blog</NavLink>
                     </li>
                     <li className={`${styles["header-nav-li"]} logo gold-underline`}>
                         <Link to="/">Wedding Planner</Link>
                     </li>
                     <li className={styles["header-nav-li"]}>
-                        <Link to="/login">Login</Link>
+                        <NavLink className={setNavStyle} to="/login">Login</NavLink>
                     </li>
                     <li className={styles["header-nav-li"]}>
-                        <Link to="/register">Register</Link>
+                        <NavLink className={setNavStyle} to="/register">Register</NavLink>
                     </li>
                 </ul>
             </nav>
