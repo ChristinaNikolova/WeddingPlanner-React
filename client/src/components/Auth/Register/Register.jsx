e5aa9ed00d1e42bdec3e352e240f7e3127d40332
christinanikolova <christinanikolova@github.com>
2022-11-05 19:49:03 +0200

    work on Register component

--- client/src/components/Auth/Register/Register.jsx
+++ client/src/components/Auth/Register/Register.jsx
@@ -2,11 +2,30 @@ import { useState } from 'react';
 import './Register.css';
 
 function Register() {
-    const [email, setEmail] = useState('');
+    const [values, setValues] = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+        repass: '',
+    });
 
     const submitHandler = (e) => {
         e.preventDefault();
+        console.log(values);
     }
+
+    const changeHandler = (e) => {
+        setValues((state) => ({
+            ...state,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    const blurHandler = (e) => {
+        console.log('blur');
+    }
+
     return (
         <section className="register section">
             <div className="register-title-wrapper">
@@ -16,29 +35,59 @@ function Register() {
                 </p>
             </div>
             <div className="register-content-wrapper">
-                <img className="register-img" src="./img/wedding-634526_1280.jpg" alt="accessories" />
+                <img className="register-img" src="./img/wedding-634526_1280.jpg" alt="bride_accessories" />
                 <form onSubmit={submitHandler} className="register-form">
                     <div className="register-form-wrapper">
                         <label htmlFor="email">Email</label>
-                        <input id="email" type="email" name="email" />
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            onChange={changeHandler}
+                            value={values.email}
+                        />
                     </div>
                     <div className="register-form-wrapper">
                         <label htmlFor="firstName">First Name</label>
-                        <input id="firstName" type="text" name="firstName" />
+                        <input
+                            id="firstName"
+                            type="text"
+                            name="firstName"
+                            onChange={changeHandler}
+                            value={values.firstName}
+                        />
                     </div>
                     <div className="register-form-wrapper">
                         <label htmlFor="lastName">Last Name</label>
-                        <input id="lastName" type="text" name="lastName" />
+                        <input
+                            id="lastName"
+                            type="text"
+                            name="lastName"
+                            onChange={changeHandler}
+                            value={values.lastName}
+                        />
                     </div>
                     <div className="register-form-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input id="password" type="password" name="password" />
+                        <input
+                            id="password"
+                            type="password"
+                            name="password"
+                            onChange={changeHandler}
+                            value={values.password}
+                        />
                     </div>
                     <div className="register-form-wrapper">
                         <label htmlFor="repass">Repeat Password</label>
-                        <input id="repass" type="password" name="repass" />
+                        <input
+                            id="repass"
+                            type="password"
+                            name="repass"
+                            onChange={changeHandler}
+                            value={values.repass}
+                        />
                     </div>
-                    <button className="btn">Register</button>
+                    <button className="btn" disabled={values}>Register</button>
                 </form>
             </div>
         </section>
