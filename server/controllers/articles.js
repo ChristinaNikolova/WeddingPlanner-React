5008c7f4011ba9421ea66120bca990f8a347e30d
christinanikolova <christinanikolova@github.com>
2022-12-10 15:44:11 +0200

    fix small issue with the current page

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -8,16 +8,11 @@ router.get('/:page/:category?', async (req, res) => {
         let currentPage = req.params.page;
         const selectedCategory = req.params.category || '';
 
-        if (selectedCategory) {
-            currentPage = 1;
-        }
-
         const skip = (currentPage - 1) * pagination.ARTICLES_PER_PAGE;
         const totalArticles = await getTotalCount(selectedCategory);
         const pagesCount = Math.ceil(totalArticles / pagination.ARTICLES_PER_PAGE);
 
         const articles = await all(pagination.ARTICLES_PER_PAGE, skip, selectedCategory);
-        console.log('articles');
         res.json({ articles, pagesCount, currentPage });
     } catch (error) {
         const message = mapErrors(error);
