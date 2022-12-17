1d734052c4ae08100e1e43e365d6b8e1e155a8a4
christinanikolova <christinanikolova@github.com>
2022-12-17 10:54:23 +0200

    extract FormArticle component

--- client/src/components/Administration/Articles/Create/CreateArticle.jsx
+++ client/src/components/Administration/Articles/Create/CreateArticle.jsx
@@ -1,179 +1,44 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import * as constants from '../../../../utils/constants/article';
-import * as helpers from '../../../../utils/helpers/form';
-import * as validator from '../../../../utils/validators/article';
 import * as articlesService from '../../../../services/articles';
-import * as categoriesService from '../../../../services/categories';
 
-import Input from '../../../shared/Tags/Input/Input';
-import TextArea from '../../../shared/Tags/TextArea/TextArea';
-import Select from '../../../shared/Tags/Select/Select';
-import ClientError from '../../../shared/Errors/ClientError/ClientError';
-import ServerError from '../../../shared/Errors/ServerError/ServerError';
-
-import styles from './CreateArticle.module.css';
+import FormArticle from '../Form/FormArticle';
 
 function CreateArticle() {
+    const formName = 'Create';
     const navigate = useNavigate();
-
-    const [values, setValues] = useState({
-        title: '',
-        content: '',
-        image: '',
-        jumboImage: '',
-        category: constants.article.DEFAUL_CATEGORY_SELECTED_ID,
-    });
-
-    const [categories, setCategories] = useState([]);
-    const [isDisabled, setIsDisabled] = useState(true);
-    const [titleError, setTitleError] = useState('');
-    const [contentError, setContentError] = useState('');
-    const [imageError, setImageError] = useState('');
-    const [jumboImageError, setJumboImageError] = useState('');
-    const [categoryError, setCategoryError] = useState('');
     const [serverError, setServerError] = useState('');
 
     useEffect(() => {
-        categoriesService
-            .all()
-            .then((data) => setCategories(data))
-            .catch((err) => console.error(err));
-    }, []);
+    }, [serverError]);
 
-    useEffect(() => {
-        checkDisabled();
-    }, [values, titleError, contentError, imageError, jumboImageError, categoryError]);
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        setTitleError(validator.validTitle(values.title));
-        setContentError(validator.validContent(values.content));
-        setImageError(validator.validImage(values.image));
-        setJumboImageError(validator.validImage(values.jumboImage));
-        setCategoryError(validator.validCategory(values.category));
-
-        if (titleError || contentError || imageError || jumboImageError || categoryError) {
-            return;
-        }
-
+    const submitHandler = (title, content, image, jumboImage, category) => {
         // todo refactor css
 
-        articlesService.create(values.title, values.content, values.image, values.jumboImage, values.category)
+        articlesService.create(title, content, image, jumboImage, category)
             .then((data) => {
                 if (data.message) {
                     setServerError(data.message);
                     return;
                 }
 
-
                 navigate(`/blog/1/${data._id}`);
             })
             .catch((err) => console.error(err));
     }
 
-    const changeHandler = (e) => {
-        setValues((state) => ({
-            ...state,
-            [e.target.name]: e.target.value,
-        }));
-    }
-
-    const validateTitle = () => {
-        setTitleError(validator.validTitle(values.title));
-    }
-
-    const validateContent = () => {
-        setContentError(validator.validContent(values.content));
-    }
-
-    const validateImage = () => {
-        setImageError(validator.validImage(values.image));
-    }
-
-    const validateJumboImage = () => {
-        setJumboImageError(validator.validImage(values.jumboImage));
-    }
-
-    const validateCatagory = () => {
-        setCategoryError(validator.validCategory(values.category));
-    }
-
-    const checkDisabled = () => {
-        setIsDisabled(helpers.isButtonDisabled(values, [titleError, contentError, imageError, jumboImageError, categoryError]));
-    };
-
     return (
-        <section className="section section-background">
-            {serverError && <ServerError errors={serverError} />}
-            <div className="section-title-wrapper">
-                <h2 className="section-title">Create Article</h2>
-            </div>
-            <div className={styles["create-article-content-wrapper"]} >
-                <form className={styles["create-article-form"]} onSubmit={submitHandler}>
-                    <div className="form-wrapper">
-                        <Input
-                            name="title"
-                            type="text"
-                            label="Title"
-                            value={values.title}
-                            onChangeHandler={changeHandler}
-                            onBlurHandler={validateTitle}
-                        />
-                        {titleError && <ClientError error={titleError} />}
-                    </div>
-                    <div className="form-wrapper">
-                        <TextArea
-                            name="content"
-                            label="Content"
-                            value={values.content}
-                            cols="30"
-                            rows="16"
-                            onChangeHandler={changeHandler}
-                            onBlurHandler={validateContent}
-                        />
-                        {contentError && <ClientError error={contentError} />}
-                    </div>
-                    <div className="form-wrapper">
-                        <Input
-                            name="image"
-                            type="url"
-                            label="Image"
-                            value={values.image}
-                            onChangeHandler={changeHandler}
-                            onBlurHandler={validateImage}
-                        />
-                        {imageError && <ClientError error={imageError} />}
-                    </div>
-                    <div className="form-wrapper">
-                        <Input
-                            name="jumboImage"
-                            type="url"
-                            label="Jumbo Image"
-                            value={values.jumboImage}
-                            onChangeHandler={changeHandler}
-                            onBlurHandler={validateJumboImage}
-                        />
-                        {jumboImageError && <ClientError error={jumboImageError} />}
-                    </div>
-                    <div className="form-wrapper">
-                        <Select
-                            name="category"
-                            label="Category"
-                            value={values.category}
-                            onChangeHandler={changeHandler}
-                            onBlurHandler={validateCatagory}
-                            categories={categories}
-                        />
-                        {categoryError && <ClientError error={categoryError} />}
-                    </div>
-
-                    <button className="btn btn-center" disabled={isDisabled}>Create</button>
-                </form>
-            </div>
-        </section>
+        <FormArticle
+            formName={formName}
+            title={''}
+            content={''}
+            image={''}
+            jumboImage={''}
+            category={''}
+            serverError={serverError}
+            onSubmitHandler={submitHandler}
+        />
     );
 }
 
