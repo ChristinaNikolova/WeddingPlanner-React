a6ca7246b2c9c188f1d13a418dbbaf04b670dadc
christinanikolova <christinanikolova@github.com>
2023-01-15 15:52:32 +0200

    remove todos

--- client/src/components/Blog/ArticleDetails/ArticleDetails.jsx
+++ client/src/components/Blog/ArticleDetails/ArticleDetails.jsx
@@ -12,8 +12,6 @@ import styles from './ArticleDetails.module.css';
 
 function ArticleDetails() {
     //todo responsive 
-    //todo dont forget update article form
-    //todo dont forget create planner form
     //todo last test!!!
 
     const { userId, isAdmin } = useContext(AuthContext);
